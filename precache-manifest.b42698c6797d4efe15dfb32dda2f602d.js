dc9d680538397d8c6c744982352cfd5e29f1d45e
mukul-rathi <mukul.rathi@gmail.com>
2020-10-23 11:51:31 +0100

    Updates

---
+++ precache-manifest.b42698c6797d4efe15dfb32dda2f602d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f398903d136c5cc0fb3b8bd506d992",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ef150d97fed74c64b98",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7c20e88de875c7cb78be",
+    "url": "/static/js/2.45cd584a.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.45cd584a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef150d97fed74c64b98",
+    "url": "/static/js/main.a98b00fd.chunk.js"
+  },
+  {
+    "revision": "9f7105331d0d7b030dc4",
+    "url": "/static/js/runtime-main.beb0bfef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
